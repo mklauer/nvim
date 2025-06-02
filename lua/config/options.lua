6d8954261c0773f42359933da2334a0bb476476c
mklauer <klauer.matthias@gmail.con>
2025-06-02 18:02:12 +0200

    clean setup

--- lua/config/options.lua
+++
@@ -1,9 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
-local config_path = vim.fn.stdpath("config")
-vim.g.python3_host_prog = config_path .. "/.venv/bin/python"
-
--- Disable LazyVim's auto formatting
-vim.g.autoformat = false
